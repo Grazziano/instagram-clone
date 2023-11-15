3fc5f9a6cb097cc618d8a489d3cfce6020033c1f
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-11-15 16:52:35 -0300

    Implement sticky header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,47 +8,49 @@ import { HomeIcon } from '@heroicons/react/20/solid';
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="/Instagram_logo.png"
-          alt="Instagram Logo"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-
-      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image
-          src="/instagram_icon.png"
-          alt="Instagram Logo"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="/Instagram_logo.png"
+            alt="Instagram Logo"
+            layout="fill"
+            className="object-contain"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
 
-      {/* Right */}
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="/vegeta.jpg"
-          alt="user-image"
-          className="h-10 rounded-full cursor-pointer"
-        />
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image
+            src="/instagram_icon.png"
+            alt="Instagram Logo"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <MagnifyingGlassIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="/vegeta.jpg"
+            alt="user-image"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
